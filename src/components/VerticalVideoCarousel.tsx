167c8b7108936841c7f30ce4062d17b7fe64b424
FragazBR <fragazmusic@gmail.com>
2025-08-11 10:00:30 -0300

    feat: adiciona 5 vídeos ao carrossel vertical

--- src/components/VerticalVideoCarousel.tsx
+++ src/components/VerticalVideoCarousel.tsx
@@ -2,6 +2,10 @@ import React, { useRef, useEffect, useState } from 'react';
 
 const videos = [
   '/videos/video1.mp4',
+  '/videos/video2.mp4',
+  '/videos/video3.mp4',
+  '/videos/video4.mp4',
+  '/videos/video5.mp4',
 ];
 
 const CARD_WIDTH = 400;
