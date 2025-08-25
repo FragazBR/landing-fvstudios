2230862503c1fb9b4cb6125a58fb1f2508825676
FragazBR <fragazmusic@gmail.com>
2025-08-25 18:43:41 -0300

    Remove vídeos 3, 4 e 5 do carrossel da HireUsSection

--- src/components/VerticalVideoCarousel.tsx
+++ src/components/VerticalVideoCarousel.tsx
@@ -5,9 +5,6 @@ import { cn } from "@/lib/utils";
 const videos = [
   '/videos/video1.mp4',
   '/videos/video2.mp4',
-  '/videos/video3.mp4',
-  '/videos/video4.mp4',
-  '/videos/video5.mp4',
 ];
 
 
